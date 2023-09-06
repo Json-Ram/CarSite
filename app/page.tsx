99dbb43b48ecee6dbd273290db6d043fe825ac1d
Json-Ram <jasonram778@gmail.com>
2023-09-06 11:13:32 -0700

    Create Model SearchBar,Fix spacing, Style SearchBars

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default async function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
           <h1 className='text-4xl font-extrabold'>
@@ -20,6 +21,7 @@ export default async function Home() {
 
         <div className='home__filters'>
           <SearchBar />
+
           <div className='home__filter-container'>
             <CustomFilter title='fuel' />
             <CustomFilter title='year' />
