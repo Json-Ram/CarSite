5c58ec4b63add3b8cafc9f7a508c5ffdd08d4320
Json-Ram <jasonram778@gmail.com>
2023-09-09 21:27:09 -0700

    Search functionality added, car images update with search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,8 @@
 'use client';
-import { useState } from "react";
+import React, { useState } from "react";
 import { SearchManufacturer } from ".";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
@@ -13,13 +14,45 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
       className='object-contain'
     />
   </button>
-)
+);
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
-  const [model, setmodel] = useState('')
+  const [model, setModel] = useState('');
+  const router = useRouter();
 
-  const handleSearch = () => {}
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(manufacturer === '' && model === '') {
+      return alert('Please fill in the search bar')
+    }
+
+    updateSearchParams(
+      model.toLowerCase(),
+      manufacturer.toLowerCase()
+    )
+  }
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search)
+
+    if(model) {
+      searchParams.set('model', model)
+    } else {
+      searchParams.delete('model')
+    }
+
+    if(manufacturer) {
+      searchParams.set('manufacturer', manufacturer)
+    } else {
+      searchParams.delete('manufacturer')
+    }
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathname)
+  }
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
@@ -43,7 +76,7 @@ const SearchBar = () => {
           type='text'
           name='model'
           value={model}
-          onChange={(e) => setmodel(e.target.value)}
+          onChange={(e) => setModel(e.target.value)}
           placeholder='Model'
           className='searchbar__input'
         />
@@ -52,6 +85,6 @@ const SearchBar = () => {
       <SearchButton otherClasses='max-sm:hidden' />
     </form>
   )
-}
+};
 
-export default SearchBar
+export default SearchBar;
--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,21 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
-  const secretKey: any = process.env.API_KEY
+export async function fetchCars(filters: FilterProps) {
+  const secretKey: any = process.env.API_KEY;
+  const { manufacturer, year, model, limit, fuel } = filters;
+
   const headers = {
     'X-RapidAPI-Key': secretKey,
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
-const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=supra', {
-  headers: headers,
-});
+  const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+    headers: headers,
+  });
 
-const result = await response.json();
+  const result = await response.json();
 
-return result;
+  return result;
 };
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
